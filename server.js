c160b36d7f1b5c976d76a8af3a6052c68e0d97c6
Gowtham <mallidi.kumar.reddy@securitas.com>
2026-02-24 14:07:28 +0000

    Fix Railway port issue

--- server.js
+++ server.js
@@ -52,4 +52,7 @@ app.use("/products", checkAuth, productRoutes);
 
 // START SERVER
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
