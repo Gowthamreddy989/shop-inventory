4ffeed3b273bcdb2e22236618382ab78adba98b6
Gowtham <mallidi.kumar.reddy@securitas.com>
2026-02-24 13:30:16 +0000

    Fix port for deployment

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ app.use(express.static("public"));
 app.use("/products", checkAuth, productRoutes);
 
 // START SERVER
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
